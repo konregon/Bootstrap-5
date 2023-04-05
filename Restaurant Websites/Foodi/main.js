9dffae662ab2e12f3ef4cf4cb116fdc8f66e1bc3
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2023-04-05 15:35:31 +0530

    banner and counters

---
+++ Restaurant Websites/Foodi/main.js
@@ -0,0 +1,40 @@
+// Active Navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+// Counters
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      stop = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, stop);
+  }
+  counter("count1", 0, 1274, 4500);
+  counter("count2", 100, 2274, 7500);
+  counter("count3", 0, 4574, 8500);
+  counter("count4", 0, 7074, 10500);
+});
